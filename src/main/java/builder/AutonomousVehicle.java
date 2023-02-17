88cb369e398d73fa6f72b64cd45a68016c2cf912
nora.xys <leuchno1@web.de>
2023-02-17 22:30:56 +0100

    implemented builder from modeling

--- src/main/java/builder/AutonomousVehicle.java
+++ src/main/java/builder/AutonomousVehicle.java
@@ -2,19 +2,115 @@ package builder;
 
 import bridge.IElectricEngine;
 
+//TODO import classes
 public class AutonomousVehicle {
-    private Chassis chassis;
-    private IElectricEngine engine;
-    //private IBattery battery; //TODO: Import class
-    //ILED[] headlights; //TODO: Import class
-    //IBrakeLight[] breaklights; //TODO: Import class
-    //IIndicator[] indicators; //TODO: Import class
-    Door[] doors;
-    ISittable[] seats;
-    Wheel[] wheels;
-    //IBrake[] brakes; //TODO: Import class
-    //IGPS[] gps; //TODO: Import class
-    //ICamera[] cameras; //TODO: Import class
-    //ILidar[] lidars; //TODO: Import class
-    UltraSonicSensor[] ultraSonics;
+    private final Chassis chassis;
+    private final IElectricEngine engine;
+    private final IBattery battery;
+    private final ILED[] headlights;
+    private final IBrakeLight[] breakLights;
+    private final IIndicator[] indicators;
+    private final Door[] doors;
+    private final ISittable[] seats;
+    private final Wheel[] wheels;
+    private final IBrake[] brakes;
+    private final IGPS[] gps;
+    private final ICamera[] cameras;
+    private final ILidar[] lidars;
+    private final UltraSonicSensor[] ultraSonics;
+
+    private AutonomousVehicle(Builder builder) {
+        chassis = builder.chassis;
+        engine = builder.engine;
+        battery = builder.battery;
+        headlights = builder.headlights;
+        breakLights = builder.breakLights;
+        indicators = builder.indicators;
+        doors = builder.doors;
+        seats = builder.seats;
+        wheels = builder.wheels;
+        brakes = builder.brakes;
+        gps = builder.gps;
+        cameras = builder.cameras;
+        lidars = builder.lidars;
+        ultraSonics = builder.ultraSonics;
+    }
+
+    public class Builder {
+        private Chassis chassis;
+        private IElectricEngine engine;
+        private IBattery battery;
+        private ILED[] headlights;
+        private IBrakeLight[] breakLights;
+        private IIndicator[] indicators;
+        private Door[] doors;
+        private ISittable[] seats;
+        private Wheel[] wheels;
+        private IBrake[] brakes;
+        private IGPS[] gps;
+        private ICamera[] cameras;
+        private ILidar[] lidars;
+        private UltraSonicSensor[] ultraSonics;
+
+        public Builder chassis(Chassis chassis){
+            return this;
+        }
+
+        public void engine (IElectricEngine engine){
+
+        }
+
+        public void battery (IBattery battery){
+
+        }
+
+        public void headlights (ILED[] lights){
+
+        }
+
+        public void brakeLights (IBrakeLight[] brakeLights){
+
+        }
+
+        public void indicators (IIndicator[] indicators){
+
+        }
+
+        public void doors (Door[] doors){
+
+        }
+
+        public void seats (ISittable[] seats){
+
+        }
+
+        public void wheels (Wheel wheels){
+
+        }
+
+        public void brakes (IBrake[] brakes){
+
+        }
+
+        public void gps (IGPS[] gps){
+
+        }
+
+        public void cameras (ICamera[] cameras){
+
+        }
+
+        public void lidars (ILidar[] lidars){
+
+        }
+
+        public void ultraSonics (UltraSonicSensor[] us){
+
+        }
+
+        public AutonomousVehicle build(){
+            return new AutonomousVehicle(this);
+        }
+    }
 }
+
--- src/main/java/builder/UltraSonicSensor.java
+++ src/main/java/builder/UltraSonicSensor.java
@@ -1,9 +1,10 @@
 package builder;
 
 import java.util.ArrayList;
-
+//import IUltraSonicSensorListener.java;
+//TODO import class
 public class UltraSonicSensor {
-    ArrayList<IUltraSonicSensorListener> listeners = new ArrayList<IUltraSonicSensorListener>(); //TODO import class
+    ArrayList<IUltraSonicSensorListener> listeners = new ArrayList<IUltraSonicSensorListener>();
 
     public void addListener(IUltraSonicSensorListener listener){
 
