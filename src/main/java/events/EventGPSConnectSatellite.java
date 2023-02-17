27bde3d46582145aa069282bac518d83ebbb4772
Daniel Huber <30466471+daniel0611@users.noreply.github.com>
2023-02-17 18:42:17 +0100

    Add facade

--- src/main/java/events/EventGPSConnectSatellite.java
+++ src/main/java/events/EventGPSConnectSatellite.java
@@ -2,4 +2,8 @@ package events;
 
 public class EventGPSConnectSatellite {
     private String frequency;
+
+    public EventGPSConnectSatellite(String frequency) {
+        this.frequency = frequency;
+    }
 }
