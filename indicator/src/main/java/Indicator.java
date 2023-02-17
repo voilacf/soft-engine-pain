c26a687bfba2962918f80a8398de866c1db4c70a
voilacf <vio.lafe.vf@gmail.com>
2023-02-17 19:01:33 +0100

    added indicator base

---
+++ indicator/src/main/java/Indicator.java
@@ -0,0 +1,17 @@
+public class Indicator implements IIndicator{
+    private IndicatorState state;
+    private IndicatorSide side;
+
+    public Indicator(){
+
+    }
+
+    public void on(IndicatorSide side){
+
+    }
+
+    public void off(IndicatorSide side){
+
+    }
+    //public void visit(IComponentVisitor visitor){}
+}
