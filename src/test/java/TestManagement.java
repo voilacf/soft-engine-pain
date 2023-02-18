fb40f08eaccf141977441a364441191b121b563a
voilacf <vio.lafe.vf@gmail.com>
2023-02-18 20:14:45 +0100

    preparations for the test-management

---
+++ src/test/java/TestManagement.java
@@ -0,0 +1,45 @@
+import factories.*;
+import org.junit.jupiter.api.*;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class TestManagement {
+    private Object batteryPort;
+    private Object brakePort;
+    private Object brakeLightPort;
+    private Object cameraPort;
+    private Object enginePort;
+    private Object gpsPort;
+    private Object indicatorPort;
+    private Object ledPort;
+    private Object lidarPort;
+
+    @BeforeEach
+    public void setup() {
+        batteryPort = BatteryFactory.build();
+        brakePort = BrakeFactory.build();
+        brakeLightPort = BrakeLightFactory.build();
+        cameraPort = CameraFactory.build();
+        enginePort = BatteryFactory.build();
+        gpsPort = GPSFactory.build();
+        indicatorPort = IndicatorFactory.build();
+        ledPort = LEDHeadLightFactroy.build();
+        lidarPort = LidarFactory.build();
+    }
+
+    @Test
+    @Order(1)
+    public void version() {
+        try {
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+}
