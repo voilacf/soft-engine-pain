8587cf664d3471d2adece6275bb9dbee6784700b
voilacf <vio.lafe.vf@gmail.com>
2023-02-18 12:27:49 +0100

    added port to brakelight

--- brakelight/src/main/java/BrakeLight.java
+++ brakelight/src/main/java/BrakeLight.java
@@ -1,17 +1,35 @@
-public class BrakeLight implements IBrakeLight{
+public class BrakeLight{
+    private static final BrakeLight instance = new BrakeLight();
+    public Port port;
     private BrakeLightState state;
 
     public BrakeLight(){
-        state = BrakeLightState.OFF;
+        port = new Port();
     }
 
-    public void on(){
+    public void innerOn(){
         state = BrakeLightState.ON;
     }
 
-    public void off(){
+    public void innerOff(){
         state = BrakeLightState.OFF;
     }
 
     //public void visit(IComponentVisitor visitor){}
+
+    public class Port implements IBrakeLight{
+
+        public void on(){
+            innerOn();
+        }
+
+        public void off(){
+            innerOff();
+        }
+
+        /*public void visit(IComponentVisitor visitor){
+            innerVisit(visitor);
+        }
+        * */
+    }
 }
