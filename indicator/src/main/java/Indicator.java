e42ed1b1cc9854e7d508ee2cc1ae9bb63a1d3f81
voilacf <vio.lafe.vf@gmail.com>
2023-02-18 12:19:48 +0100

    added port to indicator

--- indicator/src/main/java/Indicator.java
+++ indicator/src/main/java/Indicator.java
@@ -1,17 +1,37 @@
-public class Indicator implements IIndicator{
+public class Indicator{
+    private static final Indicator instance = new Indicator();
+    public Port port;
     private IndicatorState state;
     private IndicatorSide side;
 
     public Indicator(){
+        port = new Port();
+    }
+
+    public void innerOn(IndicatorSide side){
 
     }
 
-    public void on(IndicatorSide side){
+    public void innerOff(IndicatorSide side){
 
     }
 
-    public void off(IndicatorSide side){
+    //public void innerVisit(IComponentVisitor visitor){}
 
+    public class Port implements IIndicator{
+
+        public void on(IndicatorSide side){
+            innerOn(side);
+        }
+
+        public void off(IndicatorSide side){
+            innerOff(side);
+        }
+
+        /*public void visit(IComponentVisitor visitor){
+            innerVisit(visitor);
+        }
+        * */
     }
-    //public void visit(IComponentVisitor visitor){}
+
 }
