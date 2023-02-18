8315479272bfeab303cf13e209211cd2efe594a2
voilacf <vio.lafe.vf@gmail.com>
2023-02-18 12:08:56 +0100

    added port to electric engine

--- electric-engine/src/main/java/ElecticEngineNG.java
+++ electric-engine/src/main/java/ElecticEngineNG.java
@@ -1,24 +1,51 @@
-public class ElecticEngineNG implements IElectricEngine{
+public class ElecticEngineNG{
+    private static final ElecticEngineNG instance = new ElecticEngineNG();
+    public Port port;
     private ElectricEngineState state;
     private int rpm;
 
     public ElecticEngineNG(){
-        state = ElectricEngineState.OFF;
+        port = new Port();
     }
 
-    public void on(){
+    public void innerOn(){
         state = ElectricEngineState.ON;
     }
 
-    public void off(){
+    public void innerOff(){
         state = ElectricEngineState.OFF;
     }
 
-    public void increaseRPM(int deltaRPM, int seconds){
+    public void innerIncreaseRPM(int deltaRPM, int seconds){
 
     }
 
-    public void decreaseRPM(int deltaRPM, int seconds){
+    public void innerDecreaseRPM(int deltaRPM, int seconds){
+
+    }
+
+    public class Port implements IElectricEngine{
+
+        public void on(){
+            innerOn();
+        }
+
+        public void off(){
+            innerOff();
+        }
+
+        public void increaseRPM(int deltaRPM, int seconds){
+            innerIncreaseRPM(deltaRPM,seconds);
+        }
+
+        public void decreaseRPM(int deltaRPM, int seconds){
+            innerDecreaseRPM(deltaRPM,seconds);
+        }
+
+        /*public void visit(IComponentVisitor visitor){
+            innerVisit(visitor);
+        }
+        * */
 
     }
 }
