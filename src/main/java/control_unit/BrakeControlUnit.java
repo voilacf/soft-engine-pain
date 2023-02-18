ff308146df28845bdffd7c0d831f862312061faa
voilacf <vio.lafe.vf@gmail.com>
2023-02-18 17:55:30 +0100

    added port and invokeMethod() to each control unit

--- src/main/java/control_unit/BrakeControlUnit.java
+++ src/main/java/control_unit/BrakeControlUnit.java
@@ -1,16 +1,36 @@
 package control_unit;
 
-
+//import com.google.common.eventbus.Subscribe;
 import events.EventBrakeSet;
+import factories.BrakeFactory;
+
+import java.lang.reflect.Method;
 
 public class BrakeControlUnit extends Subscriber{
-    //private IBrake brake;
+    private final Object brakePort;
 
     public BrakeControlUnit(){
-        //TODO: brake factory
+        super(1);
+        brakePort = BrakeFactory.build();
     }
 
-    public void receive(EventBrakeSet event){
+    private void invokeMethod(Object brake, String brakeMethod) {
+        try{
+            Method m = brake.getClass().getMethod(brakeMethod);
+            m.invoke(brake);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
+    //@Subscriber
+    public void receive(EventBrakeSet event){
+        try {
+            Method onMethod = brakePort.getClass().getDeclaredMethod("open");
+            //DoorState result = (DoorState) onMethod.invoke(brakePort);
+            //System.out.println("receive -> doorOpen  | state : " + result);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
--- src/main/java/control_unit/IndicatorControlUnit.java
+++ src/main/java/control_unit/IndicatorControlUnit.java
@@ -1,35 +1,53 @@
 package control_unit;
 
-
+//import com.google.common.eventbus.Subscribe;
 import events.*;
+import factories.IndicatorFactory;
+import java.lang.reflect.Method;
 
 public class IndicatorControlUnit extends Subscriber{
-    //private IIndicator indicator;
+    private final Object indicatorPort;
 
     public IndicatorControlUnit(){
-        //TODO: indicator factory
+        super(1);
+        indicatorPort = IndicatorFactory.build();
     }
 
+    private void invokeMethod(Object indicator, String indicatorMethod) {
+        try{
+            Method m = indicator.getClass().getMethod(indicatorMethod);
+            m.invoke(indicator);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    //@Subscribe
     public void receive(EventLeftIndicatorOn event){
 
     }
 
+    //@Subscribe
     public void receive(EventLeftIndicatorOff event){
 
     }
 
+    //@Subscribe
     public void receive(EventRightIndicatorOn event){
 
     }
 
+    //@Subscribe
     public void receive(EventRightIndicatorOff event){
 
     }
 
+    //@Subscribe
     public void receive(EventHazardWarningOn event){
 
     }
 
+    //@Subscribe
     public void receive(EventHazardWarningOff event){
 
     }
