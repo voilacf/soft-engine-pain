5d8cef01d7eab76f946d129131f8d727e28dafb5
voilacf <vio.lafe.vf@gmail.com>
2023-02-18 12:22:41 +0100

    added port to led

--- led-headlight/src/main/java/LED.java
+++ led-headlight/src/main/java/LED.java
@@ -1,23 +1,44 @@
-public class LED implements ILED{
+public class LED{
+    private static final LED instance = new LED();
+    public Port port;
     private LEDState state;
 
     public LED(){
-        state = LEDState.OFF;
+        port = new Port();
     }
 
-    public void on(){
+    public void innerOn(){
         state = LEDState.ON;
     }
 
-    public void off(){
+    public void innerOff(){
         state = LEDState.OFF;
     }
 
-    public void dimm(){
+    public void innerDimm(){
         state = LEDState.DIMMED;
     }
 
-    public void beam(){
+    public void innerBeam(){
         state = LEDState.HIGHBEAM;
+    }
+
+    public class Port implements ILED{
+
+        public void on(){
+            innerOn();
+        }
+
+        public void off(){
+            innerOff();
+        }
+
+        public void dimm(){
+            innerDimm();
+        }
+
+        public void beam(){
+            innerBeam();
+        }
     }
 }
