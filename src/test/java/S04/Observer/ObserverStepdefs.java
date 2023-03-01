b30269f5f522587aaeb78652113da82bffbe8ce6
Adrian <adrias1516@gmail.com>
2023-03-01 21:01:54 +0100

    Finished S04 testing

--- src/test/java/S04/Observer/ObserverStepdefs.java
+++ src/test/java/S04/Observer/ObserverStepdefs.java
@@ -1,11 +1,16 @@
 package S04.Observer;
 
+import components.Door;
 import control_unit.ComponentUtils;
 import factories.ComponentFactory;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import observer.IBatteryCellTemperatureListener;
+import observer.IButtonPressedListener;
+import observer.IUltraSonicSensorListener;
+import observer.UltraSonicSensor;
+import service_center.ServiceCenter;
 
 import java.util.List;
 
@@ -13,9 +18,14 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class ObserverStepdefs {
     Object batteryCell;
-    TestListener listener;
+    TestBatteryListener listenerBattery;
+    UltraSonicSensor ultraSonicSensor;
+    TestUltrasonicListener listenerUltrasonic;
+    Door door;
+    TestDoorButtonSensorListener listenerDoorButtonSensor;
 
-    public static class TestListener implements IBatteryCellTemperatureListener {
+
+    public static class TestBatteryListener implements IBatteryCellTemperatureListener {
         public double lastTemperature = -1;
         public boolean temperatureUpdateReceived = false;
 
@@ -26,6 +36,27 @@ public class ObserverStepdefs {
         }
     }
 
+        public static class TestUltrasonicListener implements IUltraSonicSensorListener {
+        public double lastDistance = -1;
+        public boolean distanceUpdateReceived = false;
+            @Override
+            public void ultraSonicMeasurement(UltraSonicSensor sensor, double distance) {
+                lastDistance = distance;
+            distanceUpdateReceived = true;
+
+            }
+        }
+
+        public static class TestDoorButtonSensorListener implements IButtonPressedListener {
+
+            public boolean buttonPressReceived = false;
+
+            @Override
+            public void buttonPressed() {
+                buttonPressReceived = true;
+            }
+        }
+
     @Given("I have a battery cell")
     public void iHaveABatteryCell() {
         Object battery = ComponentFactory.buildBattery();
@@ -35,10 +66,17 @@ public class ObserverStepdefs {
         assert batterySubCells != null;
         List<Object> batteryCells = (List<Object>)ComponentUtils.invokeMethod(batterySubCells.get(0), "getSubUnits");
         assert batteryCells != null;
-
         batteryCell = batteryCells.get(0);
-        listener = new TestListener();
-        ComponentUtils.invokeMethod(batteryCell, "addListener", new Class[] {Object.class}, listener);
+    }
+
+        @Given("I have a battery listener")
+    public void iHaveAListener() {
+        listenerBattery = new TestBatteryListener();
+    }
+
+    @When("I add the listener to the battery cell")
+    public void iAddTheListenerToTheBatteryCell() {
+        ComponentUtils.invokeMethod(batteryCell, "addListener", new Class[] {Object.class}, listenerBattery);
     }
 
     @When("I discharge the battery")
@@ -46,9 +84,65 @@ public class ObserverStepdefs {
         ComponentUtils.invokeMethod(batteryCell, "useEnergy", new Class[] {int.class}, 1);
     }
 
-    @Then("The temperature should change")
-    public void theTemperatureShouldChange() {
-        assertTrue(listener.temperatureUpdateReceived);
-        assertNotEquals(-1, listener.lastTemperature);
+
+        @Then("The listener should receive a temperature change")
+    public void theListenerShouldReceiveATemperatureChange() {
+                assertTrue(listenerBattery.temperatureUpdateReceived);
+        assertNotEquals(-1, listenerBattery.lastTemperature);
+    }
+
+        @Given("I have an ultrasonic sensor")
+    public void iHaveAnUltrasonicSensor() {
+        ultraSonicSensor = new UltraSonicSensor(0);
+
+    }
+
+    @Given("I have a ultrasonic sensor listener")
+    public void iHaveAUltrasonicSensorListener() {
+        listenerUltrasonic = new TestUltrasonicListener();
+    }
+
+    @When("I add a listener to the sensor")
+    public void iAddAListenerToTheSensor() {
+        ultraSonicSensor.addListener(listenerUltrasonic);
+
+    }
+
+    @When("The distance changes")
+    public void theDistanceChanges() {
+        ultraSonicSensor.setDistance(5);
+    }
+
+    @Then("The listener should receive a distance change")
+    public void theListenerShouldReceiveADistanceChange() {
+        assertTrue(listenerUltrasonic.distanceUpdateReceived);
+        assertNotEquals(-1, listenerUltrasonic.lastDistance);
+    }
+
+    @Given("I have a door with a button sensor")
+    public void iHaveADoorWithAButtonSensor() {
+        ServiceCenter serviceCenter = new ServiceCenter();
+        door = new Door(serviceCenter);
+    }
+
+    @Given("I have a door button sensor listener")
+    public void iHaveADoorButtonSensorListener() {
+        listenerDoorButtonSensor = new TestDoorButtonSensorListener();
+
+    }
+
+    @When("I add a listener to the door button sensor")
+    public void iAddAListenerToTheDoorButtonSensor() {
+        door.getToggleButton().addListener(listenerDoorButtonSensor);
+    }
+
+    @When("I press the door button sensor")
+    public void iPressTheButton() {
+        door.getToggleButton().pressButton();
+    }
+
+    @Then("The listener should receive a button press")
+    public void theListenerShouldReceiveAButtonPress() {
+        assertTrue(listenerDoorButtonSensor.buttonPressReceived);
     }
 }
--- src/test/java/S04/Observer/observer.feature
+++ src/test/java/S04/Observer/observer.feature
@@ -1,5 +1,22 @@
 Feature: observer
-  Scenario: Test battery temperature change
+
+  Scenario: Test battery sensor
     Given I have a battery cell
+    Given I have a battery listener
+    When I add the listener to the battery cell
     When I discharge the battery
-    Then The temperature should change
+    Then The listener should receive a temperature change
+
+  Scenario: Test ultrasonic sensor
+    Given I have an ultrasonic sensor
+    Given I have a ultrasonic sensor listener
+    When I add a listener to the sensor
+    When The distance changes
+    Then The listener should receive a distance change
+
+  Scenario: Test door button sensor
+    Given I have a door with a button sensor
+    Given I have a door button sensor listener
+    When I add a listener to the door button sensor
+    When I press the door button sensor
+    Then The listener should receive a button press
