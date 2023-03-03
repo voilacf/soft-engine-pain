95fe1f6d9eb89a082ac24ec71e885e147a4e0569
voilacf <vio.lafe.vf@gmail.com>
2023-03-03 19:58:48 +0100

    update

--- src/main/java/factories/Factory.java
+++ src/main/java/factories/Factory.java
@@ -16,26 +16,6 @@ import java.util.HashMap;
 public class Factory {
     private static HashMap<String, URL> jarPaths;
 
-    static{
-        jarPaths = new HashMap<>();
-        try{
-            jarPaths.put("Battery",new File(Configuration.INSTANCE.pathToBatteryJavaArchive + "battery.jar").toURI().toURL());
-            jarPaths.put("Brake",new File(Configuration.INSTANCE.pathToBrakeJavaArchive + "brake.jar").toURI().toURL());
-            jarPaths.put("BrakeLight",new File(Configuration.INSTANCE.pathToBrakeLightJavaArchive + "brakelight.jar").toURI().toURL());
-            jarPaths.put("CameraV1",new File(Configuration.INSTANCE.pathToCameraJavaArchive + "camera.jar").toURI().toURL());
-            jarPaths.put("CameraV2",new File(Configuration.INSTANCE.pathToCameraJavaArchive + "camera.jar").toURI().toURL());
-            jarPaths.put("ElectricEngineNG",new File(Configuration.INSTANCE.pathToElectricEngineJavaArchive + "electric-engine.jar").toURI().toURL());
-            jarPaths.put("ElectricEngineX",new File(Configuration.INSTANCE.pathToElectricEngineJavaArchive + "electric-engine.jar").toURI().toURL());
-            jarPaths.put("GPS",new File(Configuration.INSTANCE.pathToGPSJavaArchive + "gps.jar").toURI().toURL());
-            jarPaths.put("Indicator",new File(Configuration.INSTANCE.pathToIndicatorJavaArchive + "indicator.jar").toURI().toURL());
-            jarPaths.put("LED",new File(Configuration.INSTANCE.pathToLedHeadlightJavaArchive + "led-headlight.jar").toURI().toURL());
-            jarPaths.put("LidarNG",new File(Configuration.INSTANCE.pathToLidarJavaArchive + "lidar.jar").toURI().toURL());
-            jarPaths.put("LidarXT",new File(Configuration.INSTANCE.pathToLidarJavaArchive + "lidar.jar").toURI().toURL());
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-    }
-
     private static Object build(String classname) {
         Object port = null;
         try {
@@ -52,6 +32,54 @@ public class Factory {
             e.printStackTrace();
         }
         return port;
+    }
+
+    public static Object buildBattery(){
+        return build("Battery");
+    }
+
+    public static Object buildBrake(){
+        return build("Brake");
+    }
+
+    public static Object buildBrakeLight(){
+        return build("BrakeLight");
+    }
+
+    public static Object buildCamera(CameraComponentType type){
+        if(CameraComponentType.CAMERA_V1 == type){
+            return build("CameraV1");
+        }else{
+            return build("CameraV2");
+        }
+    }
+
+    public static Object buildEngine(EngineComponentType type){
+        if(EngineComponentType.ENGINE_NG == type){
+            return build("ElectricEngineNG");
+        }else{
+            return build("ElectricEngineXT");
+        }
+    }
+
+    public static Object buildGPS(){
+        return build("GPS");
+    }
+
+    public static Object buildIndicator(){
+        return build("Indicator");
+    }
+
+    public static Object buildLED(){
+        return build("LED");
+    }
+
+    public static Object buildLidar(LidarComponentType type){
+        if(LidarComponentType.LIDAR_NG == type){
+            return build("LidarNG");
+        }else{
+            return build("LidarXT");
+        }
     }
 
     private static boolean verify(String pathToJar){
@@ -85,50 +113,23 @@ public class Factory {
         return isVerified;
     }
 
-    public static Object buildBattery(){
-        return build("Battery");
-    }
-
-    public static Object buildBrake(){
-        return build("Brake");
-    }
-
-    public static Object buildBrakeLight(){
-        return build("BrakeLight");
-    }
-
-    public static Object buildCamera(CameraComponentType type){
-        if(CameraComponentType.CAMERA_V1 == type){
-            return build("CameraV1");
-        }else{
-            return build("CameraV2");
-        }
-    }
-    public static Object buildEngine(EngineComponentType type){
-        if(EngineComponentType.ENGINE_NG == type){
-            return build("ElectricEngineNG");
-        }else{
-            return build("ElectricEngineXT");
-        }
-    }
-
-    public static Object buildGPS(){
-        return build("GPS");
-    }
-
-    public static Object buildIndicator(){
-        return build("Indicator");
-    }
-
-    public static Object buildLED(){
-        return build("LED");
-    }
-
-    public static Object buildLidar(LidarComponentType type){
-        if(LidarComponentType.LIDAR_NG == type){
-            return build("LidarNG");
-        }else{
-            return build("LidarXT");
+    static{
+        jarPaths = new HashMap<>();
+        try{
+            jarPaths.put("Battery",new File(Configuration.INSTANCE.pathToBatteryJavaArchive + "battery.jar").toURI().toURL());
+            jarPaths.put("Brake",new File(Configuration.INSTANCE.pathToBrakeJavaArchive + "brake.jar").toURI().toURL());
+            jarPaths.put("BrakeLight",new File(Configuration.INSTANCE.pathToBrakeLightJavaArchive + "brakelight.jar").toURI().toURL());
+            jarPaths.put("CameraV1",new File(Configuration.INSTANCE.pathToCameraJavaArchive + "camera.jar").toURI().toURL());
+            jarPaths.put("CameraV2",new File(Configuration.INSTANCE.pathToCameraJavaArchive + "camera.jar").toURI().toURL());
+            jarPaths.put("ElectricEngineNG",new File(Configuration.INSTANCE.pathToElectricEngineJavaArchive + "electric-engine.jar").toURI().toURL());
+            jarPaths.put("ElectricEngineX",new File(Configuration.INSTANCE.pathToElectricEngineJavaArchive + "electric-engine.jar").toURI().toURL());
+            jarPaths.put("GPS",new File(Configuration.INSTANCE.pathToGPSJavaArchive + "gps.jar").toURI().toURL());
+            jarPaths.put("Indicator",new File(Configuration.INSTANCE.pathToIndicatorJavaArchive + "indicator.jar").toURI().toURL());
+            jarPaths.put("LED",new File(Configuration.INSTANCE.pathToLedHeadlightJavaArchive + "led-headlight.jar").toURI().toURL());
+            jarPaths.put("LidarNG",new File(Configuration.INSTANCE.pathToLidarJavaArchive + "lidar.jar").toURI().toURL());
+            jarPaths.put("LidarXT",new File(Configuration.INSTANCE.pathToLidarJavaArchive + "lidar.jar").toURI().toURL());
+        }catch (Exception e){
+            e.printStackTrace();
         }
     }
 
